4658ac2453dbda93dee918fb9ae4273e6d4d9512
Falence Lemungoh <falencelemungoh@gmail.com>
2022-05-22 16:19:57 +0100

    sticky navigation part 2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  $('h1').click(function() {
-    $(this).css('background-color', '#ff0000')
-  })
+  // change h1 background color on click
+  // $('h1').click(function() {
+  //   $(this).css('background-color', '#ff0000')
+  // })
+
+  // add sticky nav on scroll down
+  $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky')
+    } else {
+      $('nav').removeClass('sticky')
+    }
+  }, {
+    offset: '60px'
+  });
+
 });
