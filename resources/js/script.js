0bc3a8811683a1e4980eb4bb866416fd9e706211
Falence Lemungoh <falencelemungoh@gmail.com>
2022-05-23 01:29:18 +0100

    responsive navigation toggler

--- resources/js/script.js
+++ resources/js/script.js
@@ -18,7 +18,9 @@ $(document).ready(function() {
 
   // Scroll on buttons
   $('.js--scroll-to-plans').click(function() {
-    $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000); // scroll to plans section with 1000ms speed
+    $('html, body').animate({ 
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000); // scroll to plans section with 1000ms speed
   });
   
   $('.js--scroll-to-start').click(function() {
@@ -42,6 +44,49 @@ $(document).ready(function() {
         }
       }
     });
+  });
+
+
+  // Animations on scroll
+  // check out Animate.css (daneden.github.io/animate.css)
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+  
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+  
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+  
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '40%'
+  });
+
+
+  // Mobile navigation
+  $('.js--nav-icon').click(function() {
+    let nav = $('.js--main-nav');
+    let icon = $('.js--nav-icon i');
+
+    nav.slideToggle(300);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round')
+      icon.removeClass('ion-close-round')
+    }
   });
 
 });
